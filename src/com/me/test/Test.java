090f3e38cfb7a0219ffe97e1227a85317486ce36
aribbean <sikefa@qq.com>
2017-09-04 13:22:01 +0800

    测试

---
+++ src/com/me/test/Test.java
@@ -0,0 +1,19 @@
+package com.me.test;
+
+/**
+ * 这是个注释
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("Hello IDEA!");
+        System.out.println("Hello IDEA!");
+        System.out.println("Hello IDEA!");
+        System.out.println("Hello IDEA!");
+        System.out.println("Hello IDEA!");
+        System.out.println("Hello IDEA!");
+        System.out.println("Hello IDEA!");
+        System.out.println("Hello IDEA!");
+    }
+
+}
